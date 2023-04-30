b550af1aeb5ef4b09ff353fc8ed0e1b50f3b8791
ritikar97 <rira3427@colorado.edu>
2023-04-30 14:15:01 -0600

    Trial

--- bme280-driver/main.c
+++ bme280-driver/main.c
@@ -116,8 +116,8 @@ static long unsigned int bme280_temp_read(void)
     var2 = (((((adc_T >> 4) - ((long signed int) dig_T1_val)) * ((adc_T >> 4) - ((long signed int) dig_T1_val))) >> 12) *
         ((long signed int) dig_T3_val)) >> 14;*/
 
-    var1 = ((((adc_T>>3) – ((long int signed)dig_T1_val<<1))) * ((long int signed)dig_T2_val)) >> 11;
-    var2 = (((((adc_T>>4) – ((long int signed)dig_T1_val)) * ((adc_T>>4) – ((long int signed)dig_T1_val))) >> 12) *
+    var1 = ((((adc_T>>3) - ((long int signed)dig_T1_val<<1))) * ((long int signed)dig_T2_val)) >> 11;
+    var2 = (((((adc_T>>4) - ((long int signed)dig_T1_val)) * ((adc_T>>4) - ((long int signed)dig_T1_val))) >> 12) *
     ((long int signed)dig_T3_val)) >> 14;   
 
     T = ((var1 + var2) * 5 + 128) >> 8;
